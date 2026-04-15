282690bfe986f1e623f26cd83ab63c5d1be5c4a1
GSoMan-dev <ayotte.victor@gmail.com>
2026-04-15 08:42:18 +0200

    corrected error in my correction of check grades again

--- gradebook/grades.py
+++ gradebook/grades.py
@@ -35,7 +35,7 @@ def add_grade(grades: dict, student_id: str, subject: str, score: int) -> dict:
         >>> db
         {"S001": {"Math": 85}}
     """
-    if 0 > score > 100:
+    if 0 < score > 100:
         print(f"Invalid score: {score}. Score must be between 0 and 100.")
     elif student_id not in grades:
         grades.update({student_id: {subject: score}})
